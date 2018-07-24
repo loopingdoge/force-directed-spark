78a3f8721de1f4bcf62e178a02268f0630948e2c
Alberto Nicoletti <albyx.n@gmail.com>
2018-07-24 22:06:24 +0200

    Updated the WordCount to have input arguments

--- src/main/scala/WordCount.scala
+++ src/main/scala/WordCount.scala
@@ -2,17 +2,24 @@ import org.apache.spark.sql.SparkSession
 
 object WordCount {
   def main(args: Array[String]) {
+    // Args parsing
+    val inPath = args(0)
+    val outPath = args(1)
+
+    // Spark initialization
     val spark = SparkSession
       .builder
       .appName("Word Count")
       .config("spark.master", "local")
       .getOrCreate()
     val sc = spark.sparkContext
-    val textFile = sc.textFile("data/pietronostro.txt")
+
+    val textFile = sc.textFile(inPath)
     val counts = textFile.flatMap(line => line.split(" "))
       .map(word => (word, 1))
       .reduceByKey(_ + _)
-    counts.saveAsTextFile("out/wordcount")
+    counts.saveAsTextFile(outPath)
+
     spark.stop()
   }
 }
