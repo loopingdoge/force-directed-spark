477051c978ebf61b778911a20bb93c94abfa4f58
Alberto Nicoletti <albyx.n@gmail.com>
2018-07-25 20:32:58 +0200

    Added Vector class

---
+++ src/main/scala/Vector.scala
@@ -0,0 +1,51 @@
+class Vec2(val x: Double, val y: Double) {
+    def length: Double =
+        Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2))
+    
+    def normalize: Vec2 = {
+        val l = this.length
+        new Vec2(x / l, y / l)
+    }
+
+    def *(s: Double): Vec2 =
+        new Vec2(x * s, y * s)
+    
+    def +(s: Vec2): Vec2 =
+        new Vec2(x + s.x, y + s.y)
+
+    override def toString: String = s"($x, $y)"
+}
+
+object Vec2 {
+    def zero: Vec2 =
+        new Vec2(0, 0)
+
+    def apply(x: Double, y: Double): Vec2 =
+        new Vec2(x, y)
+}
+
+class Vec3(val x: Double, val y: Double, val z: Double) {
+    def length: Double =
+        Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2))
+
+    def normalize: Vec3 = {
+        val l = length
+        new Vec3(x / l, y / l, z / l)
+    }
+
+    def *(s: Double): Vec3 =
+        new Vec3(x * s, y * s, z * s)
+
+    def +(s: Vec3): Vec3 =
+        new Vec3(x + s.x, y * s.y, z + s.z)
+
+    override def toString: String = s"($x, $y, $z)"
+}
+
+object Vec3 {
+    def zero: Vec3 =
+        new Vec3(0, 0, 0)
+
+    def apply(x: Double, y: Double, z: Double): Vec3 =
+        new Vec3(x, y, z)
+}
