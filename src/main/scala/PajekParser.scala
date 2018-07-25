f04cc6876dcd5bba4c98e7ef86a1d46058c11d89
Alberto Nicoletti <albyx.n@gmail.com>
2018-07-25 18:22:58 +0200

    Bugfix: The vertices of the graph were not created correctly

--- src/main/scala/PajekParser.scala
+++ src/main/scala/PajekParser.scala
@@ -5,9 +5,10 @@ import scala.io.Source
 object PajekParser {
     def parse(fileName: String): Graph[Int] = {
         var lines = Source.fromFile(fileName).getLines
-        val vertices = lines.next.split(" ")(1).toInt
+        val nVertices = lines.next.split(" ")(1).toInt
+        val vertices = (0 until nVertices).toList
         lines.next // skip *Edges line
         val edges = (lines.map(line => line.split(" ")).map(splitted => Tuple2(splitted(0).toInt, splitted(1).toInt))).toList
-        new Graph(List(vertices), edges)
+        new Graph(vertices, edges)
     }
 }
