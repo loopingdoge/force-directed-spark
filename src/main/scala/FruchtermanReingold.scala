bf1b6c3e1330b4e7aab59003eaed77ed41ad739f
Alberto Nicoletti <albyx.n@gmail.com>
2018-07-25 20:33:59 +0200

    WIP: FruchtermanReingold algorithm

--- src/main/scala/FruchtermanReingold.scala
+++ src/main/scala/FruchtermanReingold.scala
@@ -1,3 +1,7 @@
+class FRVert(x: Double, y: Double, val disp: Vec2) extends Point2D(x, y) {
+    override def toString = s"($x, $y, $disp)"
+}
+
 object FruchtermanReingold {
     def repulsiveForce(k: Double, x: Double): Double = {
         Math.pow(k, 2) / x
@@ -12,10 +16,26 @@ object FruchtermanReingold {
         val width = 800
         val length = 600
         val area = width * length
+
         // Place vertices at random
         val graph = PajekParser.parse("data/sample_graph.net")
+            .map { _ => new FRVert(Math.random() * width, Math.random() * length, Vec2.zero) }
+
         // Optimal pairwise distance
         val k = Math.sqrt(area / graph.vertices.length)
-        // TODO: il resto
+
+        // TODO: cycle
+        val graphWithRepulsion = graph.vertices.map { v =>
+            var displacement = Vec2.zero
+            for (u <- graph.vertices) {
+                if (u != v) {
+                    val delta = v - u
+                    displacement = v.disp + delta.normalize * repulsiveForce(k, delta.length)
+                }
+            }
+            new FRVert(v.x, v.y, displacement)
+        }
+
+        // TODO: continue
     }
 }
