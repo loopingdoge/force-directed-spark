a72661b2a96decaf8977263c5c9cb91b8d21c890
Cristiano Piemontese <bombayagogo@gmail.com>
2018-07-25 17:41:03 +0200

    Point changed yet again
    Generics are hard

--- src/main/scala/Point.scala
+++ src/main/scala/Point.scala
@@ -1,39 +1,32 @@
-trait Number[T] {
-    def +(other: T): T = this + other
-    def -(other: T): T = this - other
-    def *(other: T): T = this * other
-    def /(other: T): T = this / other
-}
-
-class Point2D[T <: Number[T]](var x: T, var y: T) {
-    def +(other: Point2D[T]) = new Point2D(x + other.x, y + other.y)
-    def -(other: Point2D[T]) = new Point2D(x - other.x, y - other.y)
+class Point2D(var x: Float, var y: Float) {
+    def +(other: Point2D) = new Point2D(x + other.x, y + other.y)
+    def -(other: Point2D) = new Point2D(x - other.x, y - other.y)
     // dot product
-    def **(other: Point3D[T]) = (x * other.x) + (y * other.y)
+    def **(other: Point2D) = (x * other.x) + (y * other.y)
 
     // shift and scale jointly
-    def shift(a: T) = new Point2D(x + a, y + a)
-    def scale(a: T) = new Point2D(x * a, y * a)
+    def shift(a: Float) = new Point2D(x + a, y + a)
+    def scale(a: Float) = new Point2D(x * a, y * a)
     // shift and scale independently
-    def shift(a: T, b: T) = new Point2D(x + a, y + b)
-    def scale(a: T, b: T) = new Point2D(x * a, y * b)
+    def shift(a: Float, b: Float) = new Point2D(x + a, y + b)
+    def scale(a: Float, b: Float) = new Point2D(x * a, y * b)
 
     def area() = x * y
     override def toString() = s"($x, $y)"
 }
 
-class Point3D[T <: Number[T]](var x: T, var y: T, var z: T) {
-    def +(other: Point3D[T]) = new Point3D(x + other.x, y + other.y, z + other.z)
-    def -(other: Point3D[T]) = new Point3D(x - other.x, y - other.y, z - other.z)
+class Point3D(var x: Float, var y: Float, var z: Float) {
+    def +(other: Point3D) = new Point3D(x + other.x, y + other.y, z + other.z)
+    def -(other: Point3D) = new Point3D(x - other.x, y - other.y, z - other.z)
     // dot product
-    def **(other: Point3D[T]) = (x * other.x) + (y * other.y) + (z * other.z)
+    def **(other: Point3D) = (x * other.x) + (y * other.y) + (z * other.z)
  
     // shift and scale jointly
-    def shift(a: T) = new Point3D(x + a, y + a, z + a)
-    def scale(a: T) = new Point3D(x * a, y * a, z * a)
+    def shift(a: Float) = new Point3D(x + a, y + a, z + a)
+    def scale(a: Float) = new Point3D(x * a, y * a, z * a)
     // shift and scale independently
-    def shift(a: T, b: T, c: T) = new Point3D(x + a, y + b, z + c)
-    def scale(a: T, b: T, c: T) = new Point3D(x * a, y * b, z * c)
+    def shift(a: Float, b: Float, c: Float) = new Point3D(x + a, y + b, z + c)
+    def scale(a: Float, b: Float, c: Float) = new Point3D(x * a, y * b, z * c)
 
     def volume() = x * y * z
     override def toString() = s"($x, $y, $z)"
