5197ed038302513d17218fc0d531630210024718
Alberto Nicoletti <albyx.n@gmail.com>
2018-07-26 22:56:51 +0200

    Initialization of Spark in the Main class

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,19 @@
+import org.apache.spark.sql.SparkSession
+
 object Main {
     def main(args: Array[String]) {
-        println("Runnami tutto")
+        // Spark initialization
+        val spark = SparkSession
+            .builder
+            .appName("Word Count")
+            .config("spark.master", "local[4]")
+            .getOrCreate()
+
+        val sc = spark.sparkContext
+        sc.setLogLevel("ERROR")
+
+        FruchtermanReingold.runSpark(sc, 100)
+
+        spark.stop()
     }
 }
