b4e5bc08fe308c1e97c038d10f58a2f4a0d5cbe9
Alberto Nicoletti <albyx.n@gmail.com>
2018-07-27 19:29:55 +0200

    Pajek can now read/write on Google Cloud bucket. Implemented full specification Pajek reading.

--- src/main/scala/Pajek.scala
+++ src/main/scala/Pajek.scala
@@ -1,43 +1,56 @@
 /* A parser for Pajek graph definition format */
 
 import scala.io.Source
-import org.apache.spark.graphx.{Graph => XGraph, Edge, VertexId}
 import java.io._
 
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.{FileSystem, Path}
+
 object Pajek {
+
+    val conf = new Configuration()
+//    val fs: FileSystem = new Path("gs://force-directed-bucket").getFileSystem(conf)
+    val fs: FileSystem = FileSystem.get(conf)
+
     def parse(fileName: String): Graph[Int] = {
-        var lines = Source.fromFile(fileName).getLines
+        val stream = fs.open(new Path(fileName))
+        val lines = Source.fromInputStream(stream).getLines
         val nVertices = lines.next.split(" ")(1).toInt
         val vertices = (0 until nVertices).toList
+        vertices.foreach(_ => lines.next)
         lines.next // skip *Edges line
-        val edges = (lines.map(line => line.split(" ")).map(splitted => Tuple2(splitted(0).toInt, splitted(1).toInt))).toList
+        val edges = lines.map(line => line.split(" ")).map(splitted => Tuple2(splitted(0).toInt, splitted(1).toInt)).toList
         new Graph(vertices, edges)
     }
 
-    def dump(graph: Graph[Point2], filepath: String) = {
+    def dump(graph: Graph[Point2], filepath: String): Unit = {
 
         def round(num: Double): Double = {
             BigDecimal(num).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
         }
 
-        val file = new File(filepath)
-        val bw = new BufferedWriter(new FileWriter(file))
+        val file = fs.create(new Path(filepath))
+        val bw = new PrintWriter(file)
 
-        var output = s"*Vertices ${graph.vertices.length}\n"
-        graph
+        val verticesHeading = List(s"*Vertices ${graph.vertices.length}")
+        val verticesPajek = graph
             .vertices
             .zipWithIndex
-            .foreach { case (v, i) => 
-                output += s"${i + 1} ${round(v.x)} ${round(v.y)}\n"
+            .map { case (v, i) =>
+                s"${i + 1} ${round(v.x)} ${round(v.y)}"
             }
 
-        output += "*Edges\n"
-        graph
+        val edgesHeading = List("*Edges")
+        val edgesPajek = graph
             .edges
-            .foreach {
+            .map {
                 case (u, v) =>
-                    output += s"${u} ${v}\n"
+                    s"$u $v"
             }
+
+        val output = List(verticesHeading, verticesPajek, edgesHeading, edgesPajek)
+            .flatten
+            .mkString("\n")
 
         bw.write(output)
         bw.close()
