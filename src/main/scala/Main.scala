241c2897354c554365d31ae1ababca6c4bfe4954
Alberto Nicoletti <albyx.n@gmail.com>
2018-07-27 19:32:00 +0200

    Added a function to get the calculation time

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,18 +1,35 @@
 import org.apache.spark.sql.SparkSession
 
 object Main {
+
+    def time[R](block: => R): Long = {
+        val t0 = System.currentTimeMillis()
+        val result = block
+        val t1 = System.currentTimeMillis()
+        t1 - t0
+    }
+
     def main(args: Array[String]) {
         // Spark initialization
         val spark = SparkSession
             .builder
-            .appName("Word Count")
-            .config("spark.master", "local[4]")
+            .appName("Force Directed Layout")
+            .config("spark.master", "local[2]")
             .getOrCreate()
 
         val sc = spark.sparkContext
         sc.setLogLevel("ERROR")
 
-        FruchtermanReingold.runSpark(sc, 100)
+        val calcTime = time {
+            FruchtermanReingold.runSpark(
+                sc,
+                3,
+                "data/eth-6h.net",
+                "out/eth-6h.net"
+            )
+        }
+
+        println(s"Elapsed time: $calcTime ms")
 
         spark.stop()
     }
