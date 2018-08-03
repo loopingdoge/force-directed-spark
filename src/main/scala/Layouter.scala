6b7c14c513fb1aeda614f5e68dbf42482ffa2dbc
Alberto Nicoletti <albyx.n@gmail.com>
2018-08-03 11:40:51 +0200

    Separated TimeLogger and Layout

---
+++ src/main/scala/Layouter.scala
@@ -0,0 +1,7 @@
+import org.apache.spark.SparkContext
+
+trait Layouter {
+    def start(sc: SparkContext, inFilePath: String): Graph[Point2]
+    def run(iteration: Int, graph: Graph[Point2]): Graph[Point2]
+    def end(graph: Graph[Point2], outFilePath: String)
+}
--- src/main/scala/TimeLogger.scala
+++ src/main/scala/TimeLogger.scala
@@ -1,12 +1,3 @@
-import org.apache.spark.SparkContext
-import org.apache.spark.graphx.{Graph => XGraph, Edge, VertexId}
-
-trait LogMeBabyOneMoreTime {
-    def start(sc: SparkContext, inFilePath: String): XGraph[Point2, Null]
-    def run(interation: Int, graph: XGraph[Point2, Null]): XGraph[Point2, Null]
-    def end(graph: XGraph[Point2, Null], outFilePath: String)
-}
-
 object TimeLogger {
     /*
     def log(algorithm: LogMeBabyOneMoreTime) = {
