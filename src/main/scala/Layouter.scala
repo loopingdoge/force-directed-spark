37a30edd15c051d5b15c3c3902fd0a30af66d0eb
Alberto Nicoletti <albyx.n@gmail.com>
2018-08-03 12:27:45 +0200

    Added the number of iterations to Layouter

--- src/main/scala/Layouter.scala
+++ src/main/scala/Layouter.scala
@@ -1,7 +1,7 @@
 import org.apache.spark.SparkContext
 
 trait Layouter {
-    def start(sc: SparkContext, inFilePath: String): Graph[Point2]
+    def start(sc: SparkContext, inFilePath: String, iterations: Int): Graph[Point2]
     def run(iteration: Int, graph: Graph[Point2]): Graph[Point2]
     def end(graph: Graph[Point2], outFilePath: String)
 }
