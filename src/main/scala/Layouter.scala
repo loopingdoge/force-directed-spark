40a354bacc49b1c94b5de25d0d82fb2f5ee53a20
Devid Farinelli <devid.farinelli@gmail.com>
2018-09-06 20:59:08 +0200

    Updated layouter with FileSystem

--- src/main/scala/Layouter.scala
+++ src/main/scala/Layouter.scala
@@ -1,7 +1,8 @@
 import org.apache.spark.SparkContext
+import org.apache.hadoop.fs.FileSystem
 
 trait Layouter[P, T[P] <: Graph[P]] {
-    def start(sc: SparkContext, inFilePath: String, iterations: Int): T[P]
+    def start(sc: SparkContext, fs: FileSystem, inFilePath: String, iterations: Int): T[P]
     def run(iteration: Int, graph: T[P]): T[P]
-    def end(graph: T[P], outFilePath: String)
+    def end(graph: T[P], fs: FileSystem, outFilePath: String)
 }
